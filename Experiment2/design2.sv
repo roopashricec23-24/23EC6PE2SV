a2c6fc976b3cdc834cc80340a7a70b1fc4850057
Roopashri C <roopashric.ec23@bmsce.ac.in>
2026-01-29 21:17:37 +0530

    Update design2.sv

--- Experiment2/design2.sv
+++ Experiment2/design2.sv
@@ -1,6 +1,6 @@
 // File        :mux2to1.sv
 // Author      :Roopashri.C /1BM23EC218
-// Created     :28-01-2026
+// Created     :29-01-2026
 // Module      :mux2to1
 // Project     :SystemVerilog & Verification(23EC6PE2SV)
 // Faculty     :Prof.Ajaykumar Devarapalli
