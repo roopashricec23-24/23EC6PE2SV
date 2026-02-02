7d66e8a48a301c540398c2d60f2671ce379cdd5a
Roopashri C <roopashric.ec23@bmsce.ac.in>
2026-02-02 23:00:42 +0530

    Create design.sv

---
+++ Experiment15/design.sv
@@ -0,0 +1,13 @@
+// File        :mailbox.sv
+// Author      :Roopashri.C /1BM23EC218
+// Created     :02-02-2026
+// Module      :mailbox
+// Project     :SystemVerilog & Verification(23EC6PE2SV)
+// Faculty     :Prof.Ajaykumar Devarapalli
+// Description :Packet class models randomized transaction data used for communication.
+class Packet;
+  rand bit [7:0] val;
+endclass
+
+
+ 
